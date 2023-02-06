b058f7aa6d2a21b96e1e1b0fcb45676966cec77a
José Francisco Jarquin Martínez <ex500791.jose.jarquin@mail.telcel.com>
2023-02-06 13:49:40 -0600

    vamos en el video 161

--- app/src/main/java/com/example/popcom/MainActivity.kt
+++ app/src/main/java/com/example/popcom/MainActivity.kt
@@ -11,15 +11,15 @@ import com.example.popcom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
---
+++ app/src/main/java/com/example/popcom/common/Constantes.kt
@@ -0,0 +1,7 @@
+package com.example.popcom.common
+
+class Constantes {
+    companion object{
+        public val TMDBAPI_BASE_URL=""
+    }
+}
